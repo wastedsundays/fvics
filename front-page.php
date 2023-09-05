6e62e76975b2203e2349f5c551a554e4f14e29ed
Adam Hauck <ad.hauck@gmail.com>
2023-09-05 15:12:15 -0700

    cleanup of sections on home page

--- front-page.php
+++ front-page.php
@@ -1,11 +1,7 @@
 <?php
 /**
- * The template for displaying all pages
+ * The template for displaying the front page
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -18,169 +14,218 @@ $pagenum = 114;
 
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
 	<div class="posts-carousel px-5">
-   <!--Slide One-->
-   <div class="card">
-   <img src="http://localhost/fvics/wp-content/uploads/2023/08/canmandawe-ftTsK4QinMw-unsplash-scaled.jpg" alt="alt-text">
-      <div class="card-body">
-         <h3 class="card-title">Your Post heading0</h3>
-         <p>Your Post Excerpt</p>
-         <a href="#" class="btn btn-primary">View More</a>
-      </div>
-   </div>
-   <!--Slide Two-->
-   <div class="card">
+      <!--Slide One-->
+      <div class="card">
       <img src="http://localhost/fvics/wp-content/uploads/2023/08/canmandawe-ftTsK4QinMw-unsplash-scaled.jpg" alt="alt-text">
-      <div class="card-body">
-         <h3 class="card-title">Your Post heading1</h3>
-         <p>Your Post Excerpt</p>
-         <a href="#" class="btn btn-primary">View More</a>
+         <div class="card-body">
+            <h3 class="card-title">Your Post heading0</h3>
+            <p>Your Post Excerpt</p>
+            <a href="#" class="btn btn-primary">View More</a>
+         </div>
       </div>
-   </div>
-   <!-- Slide Three -->
-   <div class="card">
-   <img src="http://localhost/fvics/wp-content/uploads/2023/08/canmandawe-ftTsK4QinMw-unsplash-scaled.jpg" alt="alt-text">
-      <div class="card-body">
-         <h3 class="card-title">Your Post heading2</h3>
-         <p>Your Post Excerpt</p>
-         <a href="#" class="btn btn-primary">View More</a>
+      <!--Slide Two-->
+      <div class="card">
+         <img src="http://localhost/fvics/wp-content/uploads/2023/08/canmandawe-ftTsK4QinMw-unsplash-scaled.jpg" alt="alt-text">
+         <div class="card-body">
+            <h3 class="card-title">Your Post heading1</h3>
+            <p>Your Post Excerpt</p>
+            <a href="#" class="btn btn-primary">View More</a>
+         </div>
       </div>
-   </div>
-   <!-- Slide Four -->
-   <div class="card">
-   <img src="http://localhost/fvics/wp-content/uploads/2023/08/canmandawe-ftTsK4QinMw-unsplash-scaled.jpg" alt="alt-text">
-      <div class="card-body">
-         <h3 class="card-title">Your Post heading3</h3>
-         <p>Your Post Excerpt</p>
-         <a href="#" class="btn btn-primary">View More</a>
-      </div>
-   </div>
-</div>
-
-<!-- 'Welcome' Section Left Side Text -->
-<section class="home-page-grid">
-   <div>
-      <h1>Welcome to the Fraser Valley Italo-Canadian Society</h1>
-      <p><?php the_field('welcome_message', $pagenum); ?></p>
-      <button>About Us</button>
-      <button>Our History</button>
-   </div>
-   <div>
-   <?php 
-      $image = get_field('welcome_image', $pagenum);
-      $size = 'full';
-      if( $image ) {
-         echo wp_get_attachment_image( $image, $size);
-      }
-   ?>
-   </div>
-</section>
-
-<!-- News section - uses two images side by side with a small gap in between.  -->
-<section class="home-page-grid">
-   <!-- this div uses the most recent news article, with the featured image -->
-   <div>
+      <!-- Slide Three -->
+      <div class="card">
       <img src="http://localhost/fvics/wp-content/uploads/2023/08/canmandawe-ftTsK4QinMw-unsplash-scaled.jpg" alt="alt-text">
-   </div>
-   <!-- this div is going to show the title and featured image of either the second most recent update, or a specific photo. tbd -->
-   <div>
+         <div class="card-body">
+            <h3 class="card-title">Your Post heading2</h3>
+            <p>Your Post Excerpt</p>
+            <a href="#" class="btn btn-primary">View More</a>
+         </div>
+      </div>
+      <!-- Slide Four -->
+      <div class="card">
       <img src="http://localhost/fvics/wp-content/uploads/2023/08/canmandawe-ftTsK4QinMw-unsplash-scaled.jpg" alt="alt-text">
+         <div class="card-body">
+            <h3 class="card-title">Your Post heading3</h3>
+            <p>Your Post Excerpt</p>
+            <a href="#" class="btn btn-primary">View More</a>
+         </div>
+      </div>
    </div>
-</section>
 
-<!-- Events Section Left Side Text -->
-<section class="home-page-grid">
-   <div>
-      <?php
-         $home_query = new WP_Query( 'pagename=home' );
-         while ( $home_query->have_posts() ) : $home_query->the_post();
-            the_content();
-         endwhile;
-         wp_reset_postdata();
-      ?>
-   </div>
-   <div>
-   <?php
-			$args = array(
-				'post_type' => 'tribe_events',
-				// 'post__not_in' => array( get_the_ID() ),
-				'posts_per_page' => 1
-			);
-			$news_query = new WP_Query( $args );
+   <!-- 'Welcome' Section -->
+   <section>
+      <div class="home-page-grid">
+         <div>
+            <h1>Welcome to the Fraser Valley Italo-Canadian Society</h1>
+            <p><?php the_field('welcome_message', $pagenum); ?></p>
+            <button>About Us</button>
+            <button>Our History</button>
+         </div>
+         <div>
+         <?php 
+            $image = get_field('welcome_image', $pagenum);
+            $size = 'full';
+            if( $image ) {
+               echo wp_get_attachment_image( $image, $size);
+            }
+         ?>
+         </div>
+      </div>
+   </section>
 
-         if ( $news_query -> have_posts() ) {
+   <!-- News Section -->
+   <section>
+      <h2>News & Updates</h2>
+      <div class="home-page-grid">
+         <!-- this div uses the most recent news article, with the featured image -->
+         <div>
+            <?php
+               $newsargs = array(
+                  'post_type' => 'post',
+                  'posts_per_page' => 1
+               );
+               $news_query = new WP_Query( $newsargs );
 
-                while ( $news_query -> have_posts() ) {
-                  $news_query -> the_post();
-              ?>
- 
-                  <?php the_post_thumbnail( 'full' );?>
-                  <h4 class="article-thumb-title"><?php the_title(); ?></h4>                    
+               if ( $news_query -> have_posts() ) {
 
-              <?php
-                }
-              ?>
-
-                          
+                  while ( $news_query -> have_posts() ) {
+                     $news_query -> the_post();
+            ?>
+         
+            <?php the_post_thumbnail( 'full' );?>
+            <h3 class="article-thumb-title"><?php the_title(); ?></h3>                    
 
             <?php
-            wp_reset_postdata();
-          } 
-          ?>
-   </div>
-</section>
+                  }
+                  wp_reset_postdata();
+               } 
+            ?>
+         </div>
+         <!-- this div is going to show the title and featured image of either the second most recent update, or a specific photo. tbd -->
+         <div>
+            <?php
+               $oldnewsargs = array(
+                  'post_type' => 'post',
+                  'offset' => 1,
+                  'posts_per_page' => 1
+               );
+               $oldnews_query = new WP_Query( $oldnewsargs );
 
-<!-- Events Section right side text -->
-<section class="home-page-grid">
-   <div>
-      <img src="http://localhost/fvics/wp-content/uploads/2023/08/canmandawe-ftTsK4QinMw-unsplash-scaled.jpg" alt="alt-text">
-   </div>
-   <div>
-      <h2>Event Galleries</h2>
-      <p><?php the_field('event_galleries_message', $pagenum); ?></p>
-      <button>See Event Galleries</button>
-   </div>
-</section>
+               if ( $oldnews_query -> have_posts() ) {
 
-<!-- Contact Section Left Side Text -->
-<section class="home-page-grid">
-   <div>
-      <h2>Our Directors</h2>
-      <p><?php the_field('directors_section_message', $pagenum); ?></p>
-      <button>Meet our directors</button>
-   </div>
-   <div>
-      <?php 
-         $image = get_field('directors_image', $pagenum);
-         $size = 'full';
-         if( $image ) {
-         echo wp_get_attachment_image( $image, $size);
-         }
-      ?>
-   </div>
-</section>
+                  while ( $oldnews_query -> have_posts() ) {
+                     $oldnews_query -> the_post();
+            ?>
+         
+            <?php the_post_thumbnail( 'full' );?>
+            <h3 class="article-thumb-title">All News</h3>                    
 
-<!-- Contact Section right side text -->
-<section class="home-page-grid">
-   <div>
-      <?php 
-         $image = get_field('contact_image', $pagenum);
-         $size = 'full';
-         if( $image ) {
-         echo wp_get_attachment_image( $image, $size);
-         }
-      ?>
-   </div>
-   <div>
-      <h2>Contact Us</h2>
-      <p><?php the_field('contact_section_message', $pagenum); ?></p>
-      <button>Contact Us</button>
-   </div>
-</section>
+            <?php
+                  }
+                  wp_reset_postdata();
+               } 
+            ?>
+         </div>
+      </div>
+   </section>
 
-	</main><!-- #main -->
+   <!-- Events Section -->
+   <section>
+
+      <div class="home-page-grid">
+         <div>
+            <?php
+               $home_query = new WP_Query( 'pagename=home' );
+               while ( $home_query->have_posts() ) : $home_query->the_post();
+                  the_content();
+               endwhile;
+               wp_reset_postdata();
+            ?>
+         </div>
+         
+         <div>
+            <?php
+               $args = array(
+                  'post_type' => 'tribe_events',
+                  'posts_per_page' => 1
+               );
+               $event_query = new WP_Query( $args );
+
+               if ( $event_query -> have_posts() ) {
+
+                  while ( $event_query -> have_posts() ) {
+                     $event_query -> the_post();
+            ?>
+         
+            <?php the_post_thumbnail( 'full' );?>
+            <h3 class="article-thumb-title"><?php the_title(); ?></h3>                    
+
+            <?php
+                  }
+                  wp_reset_postdata();
+               } 
+            ?>
+         </div>
+      </div>
+
+      <!-- Events Recaps -->
+      <div class="home-page-grid">
+         <div>
+            <!-- this needs to pull the featured image and title from the most recent recap post -->
+            <img src="http://localhost/fvics/wp-content/uploads/2023/08/canmandawe-ftTsK4QinMw-unsplash-scaled.jpg" alt="alt-text">
+         </div>
+         <div>
+            <h2>Event Galleries</h2>
+            <p><?php the_field('event_galleries_message', $pagenum); ?></p>
+            <button>See Event Galleries</button>
+         </div>
+      </div>
+
+   </section>
+
+   <!-- Contact section -->
+   <section>
+
+      <div class="home-page-grid">
+         <div>
+            <h2>Our Directors</h2>
+            <p><?php the_field('directors_section_message', $pagenum); ?></p>
+            <button>Meet our directors</button>
+         </div>
+         <div>
+            <?php 
+               $image = get_field('directors_image', $pagenum);
+               $size = 'full';
+               if( $image ) {
+               echo wp_get_attachment_image( $image, $size);
+               }
+            ?>
+         </div>
+      </div>
+
+      <div class="home-page-grid">
+         <div>
+            <?php 
+               $image = get_field('contact_image', $pagenum);
+               $size = 'full';
+               if( $image ) {
+               echo wp_get_attachment_image( $image, $size);
+               }
+            ?>
+         </div>
+         <div>
+            <h2>Contact Us</h2>
+            <p><?php the_field('contact_section_message', $pagenum); ?></p>
+            <button>Contact Us</button>
+         </div>
+      </div>
+
+   </section>
+
+</main><!-- #main -->
 
 <?php
 
