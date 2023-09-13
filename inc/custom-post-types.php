9d5ac22b729570c3c4178e17fa9a03aa0a19306c
Adam Hauck <ad.hauck@gmail.com>
2023-09-13 10:25:12 -0700

    added event gallery cpt

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -40,6 +40,47 @@ function ah_register_custom_post_types() {
   
     register_post_type( 'fvics-sponsor', $args );
    
+
+
+//Register Event Gallery/recap custom post type
+$labels = array(
+    'name'               => _x( 'Gallery', 'post type general name'  ),
+    'singular_name'      => _x( 'Gallery', 'post type singular name'  ),
+    'menu_name'          => _x( 'Galleries', 'admin menu'  ),
+    'name_admin_bar'     => _x( 'Gallery', 'add new on admin bar' ),
+    'add_new'            => _x( 'Add Gallery', 'sponsor' ),
+    'add_new_item'       => __( 'Add New Gallery' ),
+    'new_item'           => __( 'New Gallery' ),
+    'edit_item'          => __( 'Edit Gallery' ),
+    'view_item'          => __( 'View Gallery'  ),
+    'all_items'          => __( 'All Galleries' ),
+    'search_items'       => __( 'Search Galleries' ),
+    'parent_item_colon'  => __( 'Parent Gallery:' ),
+    'not_found'          => __( 'No galleries found.' ),
+    'not_found_in_trash' => __( 'No galleries found in Trash.' ),
+);
+
+$args = array(
+    'labels'             => $labels,
+    'public'             => true,
+    'publicly_queryable' => true,
+    'show_ui'            => true,
+    'show_in_menu'       => true,
+    'show_in_rest'       => true,
+    'query_var'          => true,
+    'rewrite'            => array( 'slug' => 'galleries' ),
+    'capability_type'    => 'post',
+    'has_archive'        => true,
+    'hierarchical'       => false,
+    'menu_position'      => 7,
+    'menu_icon'          => 'dashicons-layout',
+    'supports'           => array( 'title', 'editor', 'thumbnail' ),
+    // 'template'           => array( array( 'core/title' ) ),
+    // 'template_lock'      => 'all'
+);
+
+register_post_type( 'fvics-galleries', $args );
+
 }
 
 add_action( 'init', 'ah_register_custom_post_types' );
