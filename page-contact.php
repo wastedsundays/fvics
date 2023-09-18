249bd35e5a710fa4fee74a742d012e2d9c5d99ab
Adam Hauck <ad.hauck@gmail.com>
2023-09-18 15:36:55 -0700

    added page template parts for default static pages

--- page-contact.php
+++ page-contact.php
@@ -21,12 +21,12 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page-static' );
+			get_template_part( 'template-parts/content', 'page' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
-			// if ( comments_open() || get_comments_number() ) :
-				// comments_template();
-			// endif;
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
 
 		endwhile; // End of the loop.
 		?>
@@ -34,5 +34,5 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-// get_sidebar();
+get_sidebar();
 get_footer();
