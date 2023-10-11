1cc348ee5b6ffb8de6eb3c0404936b3a689ea174
Adam Hauck <ad.hauck@gmail.com>
2023-10-10 19:22:01 -0700

    styling of news content page

--- page-news.php
+++ page-news.php
@@ -11,66 +11,77 @@
  *
  * @package FVICS_by_Adam_H
  */
-
 get_header();
 ?>
 
 	<main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();?>
-
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<header class="entry-header">
-				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-			</header><!-- .entry-header -->
-		
-			<?php fvics_adamh_post_thumbnail(); ?>
-		
-			<div class="entry-content news-page-content">
-				<?php
-				the_content();
-		
-				wp_link_pages(
-					array(
-						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'fvics_adamh' ),
-						'after'  => '</div>',
-					)
-				);
+			<?php
+			while ( have_posts() ) :
 				?>
-			</div><!-- .entry-content -->
-		
-			<?php if ( get_edit_post_link() ) : ?>
-				<footer class="entry-footer">
+				<div class="hero-section">
+					<?php fvics_adamh_post_thumbnail(); ?>
+
+					<div class="card-body">
+						<?php the_title( '<h1 class="hero-title">', '</h1>' ); ?>
+					</div>
+				</div>
+				<?php
+				the_post();?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+
+
+			
+				<?php /*fvics_adamh_post_thumbnail();*/ ?>
+			
+				<div class="entry-content news-page-content">
 					<?php
-					edit_post_link(
-						sprintf(
-							wp_kses(
-								/* translators: %s: Name of current post. Only visible to screen readers */
-								__( 'Edit <span class="screen-reader-text">%s</span>', 'fvics_adamh' ),
-								array(
-									'span' => array(
-										'class' => array(),
-									),
-								)
-							),
-							wp_kses_post( get_the_title() )
-						),
-						'<span class="edit-link">',
-						'</span>'
+					the_content();
+			
+					wp_link_pages(
+						array(
+							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'fvics_adamh' ),
+							'after'  => '</div>',
+						)
 					);
 					?>
-				</footer><!-- .entry-footer -->
-			<?php endif; ?>
-		</article><!-- #post-<?php the_ID(); ?> -->
-		<?php
+				</div><!-- .entry-content -->
 			
-		endwhile; // End of the loop.
-		?>
+				<?php if ( get_edit_post_link() ) : ?>
+					<footer class="entry-footer">
+						<?php
+						edit_post_link(
+							sprintf(
+								wp_kses(
+									/* translators: %s: Name of current post. Only visible to screen readers */
+									__( 'Edit <span class="screen-reader-text">%s</span>', 'fvics_adamh' ),
+									array(
+										'span' => array(
+											'class' => array(),
+										),
+									)
+								),
+								wp_kses_post( get_the_title() )
+							),
+							'<span class="edit-link">',
+							'</span>'
+						);
+						?>
+					</footer><!-- .entry-footer -->
+				<?php endif; ?>
+			</article><!-- #post-<?php the_ID(); ?> -->
+			<?php
+				
+			endwhile; // End of the loop.
+			get_sidebar();
+			?>
 
-	</main><!-- #main -->
+		</main><!-- #main -->
 
-<?php
-get_sidebar();
+	<?php
+	// get_sidebar();
+
 get_footer();
+
